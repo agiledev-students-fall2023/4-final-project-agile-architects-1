af57aaf3afe87ad218eb7f1c441ca2a8bb6faf7a
G <gg2484@nyu.edu>
2023-10-19 15:44:28 -0400

    make the navition

---
+++ front-end/src/routes/Ingredients.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function BrowseIngredients() {
+    return (
+      <div>
+        <h1>This is the Ingredients page</h1>
+      </div>
+    );
+  }
+  
+export default BrowseIngredients;
---
+++ front-end/src/routes/MyProfile.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function MyProfile() {
+    return (
+      <div>
+        <h1>MyProfile</h1>
+      </div>
+    );
+  }
+  
+export default MyProfile;

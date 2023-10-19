0545c1a122b1d0b20752894915ddf54766205a2b
G <gg2484@nyu.edu>
2023-10-19 15:59:34 -0400

    change the order

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,16 +16,16 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/browse" element={<BrowseIngredients />} />
                 <Route path="/fridge" element={<MyFridge />} />
-                <Route path="/profile" element={<MyProfile />} />
                 <Route path="/plan" element={<PlanMeal />} />
+                <Route path="/profile" element={<MyProfile />} />
             </Routes>
 
             <nav className="Navigation-Bar">
                 <Link className="nav-item" to="/">Home</Link>
                 <Link className="nav-item" to="/browse">Browse Ingredients</Link>
                 <Link className="nav-item" to="/fridge">My Fridge</Link>
-                <Link className="nav-item" to="/profile">My Profile</Link>
                 <Link className="nav-item" to="/plan">Plan Meal</Link>
+                <Link className="nav-item" to="/profile">My Profile</Link>
             </nav>
         </Router>
     );

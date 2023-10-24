0452ac31ebe2b56ad1cf3742b04e76f84137d39b
kevin-huang-cc <cc@10-16-185-201.dynapool.wireless.nyu.edu>
2023-10-24 18:23:43 -0400

    Adding login page and top nav bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import MyFridge from './routes/MyFridge';
 import MyProfile from './routes/MyProfile';
 import PlanMeal from './routes/PlanMeal';
 import BrowseIngredients from './routes/Ingredients';
+import Login from './routes/Login';
 
 function App() {
     return (
@@ -18,16 +19,24 @@ function App() {
                 <Route path="/fridge" element={<MyFridge />} />
                 <Route path="/plan" element={<PlanMeal />} />
                 <Route path="/profile" element={<MyProfile />} />
+                <Route path="/login" element={<Login />} />
+
             </Routes>
+            <div className="Top-Nav">
+                <Link className="login-button" to="/login">Log In</Link>
+            </div>
 
             <nav className="Navigation-Bar">
                 <Link className="nav-item" to="/">Home</Link>
                 <Link className="nav-item" to="/browse">Browse Ingredients</Link>
                 <Link className="nav-item" to="/fridge">My Fridge</Link>
                 <Link className="nav-item" to="/plan">Plan Meal</Link>
-                <Link className="nav-item" to="/profile">My Profile</Link>
+
             </nav>
+
+
         </Router>
+
     );
 }
 

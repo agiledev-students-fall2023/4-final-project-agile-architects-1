3ef29b99eaf2c032ef3161932532c18762c00ca0
SiyuBi <sb7325@nyu.edu>
2023-10-28 19:02:11 -0400

    implement plan meal page

--- front-end/src/routes/PlanMeal.js
+++ front-end/src/routes/PlanMeal.js
@@ -1,11 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './PlanMeal.css';
 
 function PlanMeal() {
+    const mealTypes = ["Breakfast", "Brunch", "Lunch", "Afternoon Tea", "Dinner", "Night Snack"];
+    
+    const [date, setDate] = useState("MM/DD/Year");
+    const [currentPage, setCurrentPage] = useState(1);
+    const [meals, setMeals] = useState({
+        "Breakfast": ["Pancakes", "Fruit Salad"],
+        "Lunch": ["Grilled Chicken Sandwich", "Caesar Salad"],
+        "Dinner": ["Steak with Mashed Potatoes", "Vegetable Stir Fry"],
+    });
+
+    // ... (other functions and handlers remain the same)
+
     return (
-      <div>
-        <h1>Plan Meal</h1>
-      </div>
+        <div className="container">
+            <header className="header">
+                <Link to="/fridge" className="back-button">Back</Link>
+                <h1 className="notebook-line">Plan Meals</h1>
+                <button onClick={() => { /* Implement pencil/edit button logic here */ }}>✎</button>
+            </header>
+
+            <div className="meal-box">
+                <h2 className="notebook-line">{date}</h2>
+                {mealTypes.map((type) => (
+                    meals[type] && meals[type].length ? (
+                        <div key={type}>
+                            <strong className="notebook-line">{type}</strong>
+                            {meals[type].map((recipe, index) => (
+                                <p key={index} className="notebook-line">{recipe}</p>
+                            ))}
+                        </div>
+                    ) : null
+                ))}
+            </div>
+
+            <div className="footer">
+                <button onClick={() => { /* Implement recommend recipe button logic here */ }}>
+                    Recommend Recipe
+                </button>
+                <div>
+                    <button /*onClick={handlePrevPage}*/>◀</button>
+                    <button /*onClick={handleNextPage}*/>▶</button>
+                </div>
+            </div>
+        </div>
     );
-  }
-  
+}
+
 export default PlanMeal;

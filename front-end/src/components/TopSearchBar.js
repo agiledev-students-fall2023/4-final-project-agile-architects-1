20694286c1d735ba1741800da72ada817de58a91
G <gg2484@nyu.edu>
2023-10-31 23:58:23 -0400

    adjust top bar

--- front-end/src/components/TopSearchBar.js
+++ front-end/src/components/TopSearchBar.js
@@ -1,24 +1,15 @@
 import React from 'react';
+import './TopSearchBar.css';
 
-const TopSearchBar = () => {
+const TopSearchBar = ({location, searchStr}) => {
     return (
         <div className="top-search-bar">
-            <div className="location">
-                <label htmlFor="location">Location:</label>
-                <input type="text" id="location" name="location" placeholder="Enter location" />
+            <div className="top-bar-location">
+                <label htmlFor="location">{location}</label>
             </div>
-            <div className="search-box">
-                <label htmlFor="search">Search:</label>
-                <input type="text" id="search" name="search" placeholder="Search for something" />
-            </div>
-            <div className="filter">
-                <label htmlFor="filter">Filter:</label>
-                <select id="filter" name="filter">
-                    <option value="">All</option>
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                </select>
+            <div className="top-bar-search">
+                <label htmlFor="search"></label>
+                <input className="top-bar-search-box" type="text" id="search" name="search" placeholder="Search for something" />
             </div>
             <div className="blank"></div>
         </div>

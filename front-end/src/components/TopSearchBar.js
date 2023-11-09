69e4677d890f3e96b168ac647d69a593c8afc0a5
Gong2047 <gg2484@nyu.edu>
2023-11-09 11:16:55 -0500

    added search bar

--- front-end/src/components/TopSearchBar.js
+++ front-end/src/components/TopSearchBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { FaLocationDot } from 'react-icons/fa6';
 import './TopSearchBar.css';
 
 const TopSearchBar = ({location, searchStr}) => {
     return (
         <div className="top-search-bar">
             <div className="top-bar-location">
+                <div className="top-bar-location-icon">
+                    <FaLocationDot />
+                </div>
                 <label htmlFor="location">{location}</label>
             </div>
             <div className="top-bar-search">
                 <label htmlFor="search"></label>
-                <input className="top-bar-search-box" type="text" id="search" name="search" placeholder="Search for something" />
+                
+                <input className="top-bar-search-box" type="text" id="search" name="search" placeholder="Search for ingredients" />
             </div>
             <div className="blank"></div>
         </div>

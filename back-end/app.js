96d8e4795ef4cc17ec17535395721b67680a1b1f
Gong2047 <gg2484@nyu.edu>
2023-11-14 12:05:21 -0500

    add logging

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import homeRoutes from './routes/homeRoutes.js';
 import BrowseRoutes from './routes/BrowseRoutes.js';
@@ -9,8 +10,12 @@ import fridgeRoutes from './routes/fridgeRoutes.js';
 
 const app = express();
 
+// Logging middleware
+app.use(morgan('dev'));
+
 // Middleware to parse JSON
 app.use(express.json());
+
 app.use(cors());
 app.use("/static", express.static("public"));
 

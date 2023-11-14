02301de744c40df5dd034cf02fa2bcab1e07888f
Gong2047 <gg2484@nyu.edu>
2023-11-14 13:02:08 -0500

    done unit test for browse

--- back-end/testServer.js
+++ back-end/testServer.js
@@ -1,10 +1,10 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3001;
+const PORT = 8000;
 
 // Start server
-app.listen(PORT, () => {
+const listener = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-export default app;
+export default listener;

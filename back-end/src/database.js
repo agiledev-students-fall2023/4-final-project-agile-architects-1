41762462bc251137e919656adb2f5e8cf14f4c2c
Gong2047 <gg2484@nyu.edu>
2023-11-28 11:57:48 -0500

    database

---
+++ back-end/src/database.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+const databaseString = process.env.database_url || "";
+
+const connectDB = async () => {
+    try {
+        // await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(databaseString);
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+export default connectDB;

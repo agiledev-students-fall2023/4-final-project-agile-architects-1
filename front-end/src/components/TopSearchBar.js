411309eb0f39f9bda0538a65f36a6bab18dea593
kevin-huang-cc <cc@CCs-MacBook-Pro.cable.rcn.com>
2023-12-18 13:41:17 -0500

    Adding post filter

--- front-end/src/components/TopSearchBar.js
+++ front-end/src/components/TopSearchBar.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { FaLocationDot } from 'react-icons/fa6';
 import './TopSearchBar.css';
 
-const TopSearchBar = ({location, searchStr}) => {
+const TopSearchBar = ({ location, searchStr, setSearchStr }) => {
 
     const onFocus = (event) => {
         event.target.setAttribute('autocomplete', 'AD33S893EA');
         console.log(event.target.autocomplete);
-      }
+      };
+
+      const handleSearchChange = (event) => {
+        setSearchStr(event.target.value);
+    };
+
     return (
         <div className="top-search-bar">
             <div className="top-bar-location">
@@ -19,7 +24,14 @@ const TopSearchBar = ({location, searchStr}) => {
             <div className="top-bar-search">
                 <label htmlFor="search"></label> 
                 
-                <input className="top-bar-search-box" id="search" name="search" placeholder="Search for ingredients" autoComplete="new-password" onFocus={onFocus} />
+                <input 
+                className="top-bar-search-box" 
+                id="search" 
+                name="search" 
+                placeholder="Search for ingredients" 
+                onFocus={onFocus} 
+                value={searchStr}
+                onChange={handleSearchChange}/>
             </div>
             <div className="blank"></div>
         </div>

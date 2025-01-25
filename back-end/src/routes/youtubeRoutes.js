f4c79de6446ebb6ec855351cc082f643a3ac05dc
kevin-huang-cc <cc@CCs-MacBook-Pro.local>
2025-01-25 10:50:56 -0500

    Refactoring the flask backend and adding API URL to environment file

--- back-end/src/routes/youtubeRoutes.js
+++ back-end/src/routes/youtubeRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import axios from 'axios'; // Use axios to call Flask API
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const youtubeRoutes = express.Router();
 
 youtubeRoutes.get('/get-videos', async (req, res) => {
     try {
-        const flaskResponse = await axios.get('http://localhost:5000/get-videos'); // Flask API URL
+        const flaskResponse = await axios.get(`${process.env.FLASK_API_URL}/get-videos`); // Flask API URL
         res.status(200).json(flaskResponse.data); // Forward Flask response to the client
     } catch (error) {
         console.error('Error fetching YouTube data:', error);
